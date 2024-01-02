3ef0678e036a490a0cb2555da7a2a856b31492c6
akoredera <akoredera@gmail.com>
2024-01-02 13:33:37 -0800

    modified 100-print_tebahpla.py file

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -3,7 +3,8 @@ j = 25
 for i in range(97, 123):
     k = i + j
     if k % 2 != 0:
-        print("{}".format(chr(k-32)), end="")
-    else:
-        print("{}".format(chr(k)), end="")
+        value = k - 32
+    else: 
+        value = k
+    print("{}".format(chr(value)), end="")
     j -= 2
