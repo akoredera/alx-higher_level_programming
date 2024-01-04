1de88b00e74d0a19e6e6fb938fc92b9cd8d8f099
akoredera <akoredera@gmail.com>
2024-01-04 06:07:30 -0800

    add 100-my_calculator.py file

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    from calculator_1 import add, sub, mul, div
+
+    try:
+        if sys.argv[1] == "" or len(sys.argv) == 4:
+            pass
+    except IndexError:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
+    else:
+        val1 = sys.argv[1]
+        val2 = sys.argv[3]
+        if sys.argv[2] == '+':
+            print(f"{val1} + {val2} = {add(val1, val2)}")
+            sys.exit(0)
+        elif sys.argv[2] == '-':
+            print(f"{val1} - {val2} = {sub(val1, val2)}")
+            sys.exit(0)
+        elif sys.argv[2] == '*':
+            print(f"{val1} * {val2} = {mul(val1, val2)}")
+            sys.exit(0)
+        elif sys.argv[2] == '/':
+            print(f"{val1} / {val2} = {div(val1, val2)}")
+            sys.exit(0)
+        else:
+            print("Unknown operator. Available operators: +, -, * and /")
+            sys.exit(1)
