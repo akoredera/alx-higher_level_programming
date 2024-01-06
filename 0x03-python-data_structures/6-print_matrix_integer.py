b070b043ae6bfe6fcd12ff616983e8c150255b2b
akoredera <akoredera@gmail.com>
2024-01-05 22:02:55 -0800

    modified 6-print_matrix_integer.py file

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,6 +3,6 @@ def print_matrix_integer(matrix=[[]]):
     for i in matrix:
         for j in i:
             if j == i[-1]:
-                print("{}$".format(j))
+                print("{:d}$".format(j))
                 break
-            print("{} ".format(j), end="")
+            print("{:d} ".format(j), end="")
