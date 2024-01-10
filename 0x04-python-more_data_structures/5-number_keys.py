0c06e430f20fce9f9471b36388dbd8ee64517999
akoredera <akoredera@gmail.com>
2024-01-10 03:38:19 -0800

    modified 5-number_keys.py file

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -2,5 +2,4 @@
 
 def number_keys(a_dictionary):
     """a function returns the number of keys in a dictionary"""
-    if a_dictionary:
-        return len(a_dictionary.keys())
+    return len(a_dictionary.keys())
