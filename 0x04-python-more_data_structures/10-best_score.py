7695bc0f7a4e3a697975e330de1df3779df2a847
akoredera <akoredera@gmail.com>
2024-01-10 03:49:48 -0800

    modified 10-best_score.py file

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,5 +2,6 @@
 
 def best_score(a_dictionary):
     '''function returns a key with the biggest integer value.'''
-    new_dic = sorted(a_dictionary)
-    return new_dic[-1]
+    if a_dictionary:
+        new_dic = sorted(a_dictionary.keys())
+        return new_dic[-1]
