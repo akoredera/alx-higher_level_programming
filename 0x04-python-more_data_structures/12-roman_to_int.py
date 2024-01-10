083323ba28a5aafd61172067c19833f6fa31430a
akoredera <akoredera@gmail.com>
2024-01-10 14:24:42 -0800

    modified 12-roman_to_int.py file

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,7 +2,7 @@
 
 def roman_to_int(roman_string):
     '''function converts a Roman numeral to an integer.'''
-    if roman_string is None:
+    if roman_string == "":
         return 0
     rom_num = dict(I=1, V=5, X=10, L=50, C=100, D=500, M=1000)
     actual = 0
