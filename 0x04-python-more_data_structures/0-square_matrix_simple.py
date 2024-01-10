2baaf1559cee1fc4b8fe8366f09d1bf006469050
akoredera <akoredera@gmail.com>
2024-01-10 03:27:51 -0800

    modified 0-square_matrix_simple.py file

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -3,7 +3,7 @@
 def square_matrix_simple(matrix=[]):
     '''function that computes the square value of all integers of a matrix.'''
     matrix_copy = matrix.copy()
-    for i in range(0, len(matrix) - 1):
+    for i in range(0, len(matrix)):
         matrix_copy[i] = list(map((lambda x: x ** 2), matrix_copy[i]))
 
     return matrix_copy
