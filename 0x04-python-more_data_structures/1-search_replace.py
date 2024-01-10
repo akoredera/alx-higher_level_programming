5f2f3b1904c955390fc95b72f4c3d55fc4eed141
akoredera <akoredera@gmail.com>
2024-01-10 03:32:29 -0800

    modified 1-search_replace.py file

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -3,7 +3,6 @@
 def search_replace(my_list, search, replace):
     """a function that replaces all occurrences
     of an element by another in a new list"""
-    if my_list:
-        new_list = my_list[:]
-        new_list = list(map(lambda x: replace if x == search else x, my_list))
-        return new_list
+    new_list = my_list[:]
+    new_list = list(map(lambda x: replace if x == search else x, my_list))
+    return new_list
