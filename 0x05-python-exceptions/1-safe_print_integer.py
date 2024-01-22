f503cd8813134f3d3bcfb80867f6032f4bfd3330
akoredera <akoredera@gmail.com>
2024-01-22 03:15:48 -0800

    add 1-safe_print_integer.py file

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""function that prints an integer with "{:d}".format()."""
+
+
+def safe_print_integer(value):
+    try:
+        if isinstance(value, int):
+                print("{:d}".format(value))
+                return True
+        raise Exception
+    except Exception:
+        return False
