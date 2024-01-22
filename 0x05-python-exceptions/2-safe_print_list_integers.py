6d01fda986f23542165679d8e7662765676ba721
akoredera <akoredera@gmail.com>
2024-01-22 03:59:07 -0800

    modified 2-safe_print_list_integers.py file

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -7,10 +7,13 @@ elements of a list and only integers.
 
 def safe_print_list_integers(my_list=[], x=0):
     count = 0
-    for i in range(0, x):
-        if not isinstance(my_list[i], int):
-            continue
-        print(my_list[i], end="")
-        count += 1
-    print("")
-    return (count)
+    try:
+        for i in range(0, x):
+            if not isinstance(my_list[i], int):
+                continue
+            print(my_list[i], end="")
+            count += 1
+            print("")
+            return (count)
+    except IndexError as err:
+        print(err)
