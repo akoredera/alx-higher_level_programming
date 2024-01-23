6339d8696ed56ef2f3efa2adca828a66a5466c47
akoredera <akoredera@gmail.com>
2024-01-23 12:02:32 -0800

    modified 1-square.py file

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,3 +1,13 @@
+#!/usr/bin/python3
+
+'''file contain a class that defines a square'''
+
+
 class Square:
+    '''class square defines a square'''
     def __init__(self, size):
+        '''initialization of object attribute
+        Args
+        size: size of a square
+        '''
         self.__size = size
