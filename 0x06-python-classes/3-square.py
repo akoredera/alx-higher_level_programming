1b11ef0f242e53394e239a4f2f67c3dae3b469b9
akoredera <akoredera@gmail.com>
2024-01-23 13:23:08 -0800

    add 3-square.py file

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+'''file contain a class that defines a square'''
+
+
+class Square:
+    '''class square defines a square'''
+    def __init__(self, size=0):
+        '''initialization of object attribute
+        Args
+        size: size of a square
+        '''
+        self.__size = size
+        if not type(self.__size) == int:
+            raise TypeError("size must be an integer")
+        elif self.__size < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        return self.__size ** 2
