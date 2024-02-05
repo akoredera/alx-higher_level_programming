a72e47072cbba5f0e08901fbb7981f4376dbc900
akoredera <akoredera@gmail.com>
2024-02-05 08:32:19 -0800

    modified 100-my_int.py file

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -3,9 +3,12 @@
 
 
 class MyInt(int):
+    '''My int inherits from int and is a rebel'''
 
     def __ne__(self, value):
+        '''method override its parent method return __eq_ method'''
         return super().__eq__(value)
 
     def __eq__(self, value):
+        '''method override its parent method return __ne__ method'''
         return super().__ne__(value)
