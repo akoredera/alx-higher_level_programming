8484b9db21f75bf3df3157d21a140e08dc916fc6
akoredera <akoredera@gmail.com>
2024-02-05 04:33:06 -0800

    modified 1-my_list.py file

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -6,10 +6,6 @@ contains the MyList class
 
 class MyList(list):
     """a child that inherit from the parent list class"""
-    def __init__(self):
-        """initialization of self and the parent class"""
-        super().__init__(self)
-
     def print_sorted(self):
         '''method print sorted list'''
         result = sorted(self)
