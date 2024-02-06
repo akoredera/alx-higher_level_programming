964d4d59780f73837311f8fd1caf24781bc9d24a
akoredera <akoredera@gmail.com>
2024-02-06 03:37:44 -0800

    modified file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,5 +6,5 @@ Read file
 
 def read_file(filename=""):
     '''reads a text file (UTF8) and prints it to stdout'''
-    with open(filename, encoding="UTF-8") as fileOpen:
+    with open(filename, 'r', encoding="UTF-8") as fileOpen:
         print(fileOpen.read())
