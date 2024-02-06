c6df7d08e9f1aad5a50099fc98dc17f872433ce8
akoredera <akoredera@gmail.com>
2024-02-06 13:02:39 -0800

    modified 7-add_item file

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -3,12 +3,13 @@
 load, add, save
 '''
 import sys
+if __name__ == "__main__":
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-n = len(sys.argv)
-arg_list = []
-for i in range(1, n):
-    arg_list.append(sys.argv[i])
-save_to_json_file(arg_list, 'add_item.json')
-load_from_json_file('add_item.json')
+    n = len(sys.argv)
+    arg_list = []
+    for i in range(1, n):
+        arg_list.append(sys.argv[i])
+    save_to_json_file(arg_list, 'add_item.json')
+    load_from_json_file('add_item.json')
