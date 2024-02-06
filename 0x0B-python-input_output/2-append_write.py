67d57ffab94c14aec461c4d260a5adfcc4e82575
akoredera <akoredera@gmail.com>
2024-02-06 04:35:41 -0800

    modified 2-append_write file

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -7,7 +7,7 @@ Append to a file
 def append_write(filename="", text=""):
     '''
     appends a string at the end of a text
-    file (UTF8) and returns the number 
+    file (UTF8) and returns the number
     of characters added
     '''
     with open(filename, 'a', encoding="UTF-8") as fileOpen:
